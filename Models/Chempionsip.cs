06b5c86959321609dbe63707513c5c5a71f9b9b2
levchenkokirill040700 <158365352+levchenkokirill040700@users.noreply.github.com>
2024-08-05 22:40:25 +0300

    Improved UX

---
+++ Models/Chempionsip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChampionshipOfBox.Models
+{
+    public class Chempionsip
+    {
+        public int Id { get; set; }
+        public Boxer BoxerWinner { get; set; }
+        public Boxer BoxerLoser { get; set; }
+        public int AmountOfRounds { get; set; }
+        public int RefereePoints { get; set; }
+        public DateTime DateOfBattle  { get; set; }
+    }
+}
---
+++ Models/СhempionsipContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChampionshipOfBox.Models
+{
+    public class СhempionsipContext : DbContext
+    {
+        public DbSet<Boxer> Boxers { get; set; }
+        public DbSet<Chempionsip> Chempionsips { get; set; }
+    }
+}
